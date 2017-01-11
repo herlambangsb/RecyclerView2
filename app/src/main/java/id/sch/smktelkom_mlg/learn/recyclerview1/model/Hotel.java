4a3948a9298d3201c13bc2a9350c723be602d92e
herlambangsb <kuraninja72@gmail.com>
2017-01-11 13:00:57 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/model/Hotel.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by SUNARI on 11/01/2017.
+ */
+public class Hotel {
+
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+
+    }
+}
